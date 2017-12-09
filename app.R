49ccfda471e1f5143102e4d26b6336aa96930b7f
GVL <gregvanlunteren@gmail.com>
2017-12-08 17:33:53 -0800

    Initial Commit, Shiny works, graph displays with reactive input

---
+++ app.R
@@ -0,0 +1,44 @@
+#CARMELO App v1
+#Plot player ages distribution on each team
+#Calculate BMI for each player
+#Temp of home city vs team city
+library(shiny)
+library(tidyverse)
+NBArosters <- read_csv("NBARosters.csv")
+NBArosters <- NBArosters %>% mutate(
+  kg = NBArosters$`#Weight`*0.45,
+  inches = sapply(strsplit(NBArosters$`#Height`,"'|\""),
+                  function(x){12*as.numeric(x[1]) + as.numeric(x[2])}),
+  meters = inches*0.025,
+  BMI = kg/(meters*meters)
+)
+allTeams <- unique(NBArosters$`#Team Abbr.`)
+
+#############################
+
+ui <- fluidPage(
+
+ pageWithSidebar(
+   headerPanel('Height vs Weight of NBA Players'),
+   sidebarPanel(
+     selectInput("select", label = h3("Team"), 
+                 choices = allTeams, 
+                 selected = "CLE")
+   ),
+   mainPanel(
+     plotOutput('value')
+   )
+ )
+)
+
+#############################
+
+server <- function(input, output) {
+  output$value <- renderPlot({
+    ggplot(data = filter(NBArosters, `#Team Abbr.` == input$select),aes(`#Height`,`#Weight`,color=`#Position`)) + geom_point()
+    #input$select is your variable for picking team name 
+    })
+}
+
+shinyApp(ui = ui, server = server)
+
