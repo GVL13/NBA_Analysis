a8ee441bdb21edc456d7e63079bc41e9752c0362
GVL <gregvanlunteren@gmail.com>
2017-12-09 20:12:24 -0800

    initial work creating a dynamic plot based on UI selection

--- app.R
+++ app.R
@@ -19,18 +19,23 @@ allTeams <- c("All",unique(NBArosters$`#Team Abbr.`))
 ui <- fluidPage(
 
  pageWithSidebar(
-   headerPanel('Height vs Weight of NBA Players'),
+   headerPanel("Analysis of NBA Rosters"),
    sidebarPanel(
-     selectInput("select", label = h3("Team"), 
-                 choices = allTeams, 
-                 selected = "CLE")
+     selectInput("type", label = h3("Analysis Type"), 
+                 choices = c("Height vs Weight by Position","BMI vs Age and Position"), 
+                 selected = "Height vs Weight by Position")
    ),
+   selectInput("subDecision", label = h3(
+     if(Input$type=="Height vs Weight by Position")
+     {"Height vs Weight by Position"}
+     else {"BMI vs Age and Position"}
+     ), choices = c("All",allTeams),selected = "CLE"),
    mainPanel(
-     plotOutput('value')
+     plotOutput(if(Input$type=="Height vs Weight by Position"){'value'}
+                else {'value2'})
    )
  )
 )
-
 
 #############################
 
@@ -41,6 +46,12 @@ server <- function(input, output) {
            aes(meters,`#Weight`,color=`#Position`)) + geom_point()
     #input$select is your variable for picking team name 
     })
+  output$value2 <- renderPlot({
+    ggplot(data = if(input$select!="All") {filter(NBArosters, `#Team Abbr.` == input$select)}
+           else {NBArosters},
+           aes(BMI,`#Age`,color=`#Position`)) + geom_point()
+    #input$select is your variable for picking team name 
+  })
 }
 
 shinyApp(ui = ui, server = server)
